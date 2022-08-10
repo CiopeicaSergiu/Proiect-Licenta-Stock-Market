8e76f836e480fb05faed9f4b78f2177ab793813f
CiopeicaSergiu <sergiu-andrei.ciopeica@student.tuiasi.ro>
2022-08-10 16:00:02 +0300

    Librarii aditionale

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,9 @@
+
+// uses curlpp, boost
 #include <curlpp/Options.hpp>
 #include <curlpp/cURLpp.hpp>
 #include <iostream>
 #include <sstream>
-
 int main() {
   // RAII cleanup
 
@@ -10,13 +11,14 @@ int main() {
 
   // Send request and get a result.
   // Here I use a shortcut to get it in a string stream ...
-  std::string urlStrig{"https://query1.finance.yahoo.com/v8/finance/chart/"
-                       "aapl?metrics=high?&interval=1d&range=5d"};
+  std::string urlStrig{"https://query1.finance.yahoo.com/v11/finance/"
+                       "quoteSummary/AAPL?modules=defaultKeyStatistics"};
 
   std::ostringstream osBuffer;
   osBuffer << curlpp::options::Url(urlStrig);
 
   const auto information = osBuffer.str();
   std::cout << information << std::endl;
+
   return 0;
 }
