e488960fa430bce9e5b7a9eb5c6afb9024d1dd43
CiopeicaSergiu <sergiu-andrei.ciopeica@student.tuiasi.ro>
2022-08-27 12:52:06 +0300

    Am reparat convertorul din data in unix timestamp

--- source/converter/TimeConverter.cpp
+++ source/converter/TimeConverter.cpp
@@ -1,9 +1,9 @@
 #include "TimeConverter.h"
 #include <boost/algorithm/string.hpp>
 #include <boost/algorithm/string/classification.hpp>
+#include <ctime>
 #include <map>
 #include <stdexcept>
-#include <time.h>
 
 auto converter::dateTime::unixTimeToDate(time_t unixTime) -> std::string {
   return "";
@@ -11,12 +11,8 @@ auto converter::dateTime::unixTimeToDate(time_t unixTime) -> std::string {
 
 auto converter::dateTime::dateToUnixTime(const std::string &date) -> time_t {
 
-  time_t rawtime;
-  struct tm *timeinfo;
-
-  std::map<std::string, size_t> days = {{"Sun", 0}, {"Mon", 1}, {"Tue", 2},
-                                        {"Wed", 3}, {"Thu", 4}, {"Fri", 5},
-                                        {"Sat", 6}};
+  setenv("TZ", "", 1);
+  std::tm timeinfo{};
 
   std::map<std::string, size_t> months = {
       {"Jan", 0}, {"Feb", 1}, {"Mar", 2}, {"Apr", 3}, {"May", 4},  {"Jun", 5},
@@ -29,12 +25,6 @@ auto converter::dateTime::dateToUnixTime(const std::string &date) -> time_t {
     throw std::runtime_error{"The date is not formatted properly!"};
   }
 
-  const auto itDays = days.find(splitedDate[0]);
-
-  if (itDays == days.end()) {
-    throw std::runtime_error{"The date is not formatted properly!"};
-  }
-
   const auto itMonths = months.find(splitedDate[1]);
 
   if (itMonths == months.end()) {
@@ -43,25 +33,19 @@ auto converter::dateTime::dateToUnixTime(const std::string &date) -> time_t {
 
   int year = std::stoi(splitedDate[6]);
   int month = itMonths->second;
-  int day = itDays->second;
+  int day = std::stoi(splitedDate[2]);
 
   int hour = std::stoi(splitedDate[3]);
   int min = std::stoi(splitedDate[4]);
   int sec = std::stoi(splitedDate[5]);
 
-  /* get current timeinfo: */
-  time(&rawtime); // or: rawtime = time(0);
-  /* convert to struct: */
-  timeinfo = localtime(&rawtime);
+  timeinfo.tm_year = year - 1900;
+  timeinfo.tm_mon = month;
+  timeinfo.tm_mday = day;
+  timeinfo.tm_hour = hour;
+  timeinfo.tm_min = min;
+  timeinfo.tm_sec = sec;
+  timeinfo.tm_isdst = 0;
 
-  /* now modify the timeinfo to the given date: */
-  timeinfo->tm_year = year - 1900;
-  timeinfo->tm_mon = month; // months since January - [0,11]
-  timeinfo->tm_mday = day;  // day of the month - [1,31]
-  timeinfo->tm_hour = hour; // hours since midnight - [0,23]
-  timeinfo->tm_min = min;   // minutes after the hour - [0,59]
-  timeinfo->tm_sec = sec;   // seconds after the minute - [0,59]
-
-  /* call mktime: create unix time stamp from timeinfo struct */
-  return mktime(timeinfo);
+  return mktime(&timeinfo);
 }
--- source/main.cpp
+++ source/main.cpp
@@ -6,6 +6,7 @@
 #include <ctime>
 #include <curlpp/Options.hpp>
 #include <curlpp/cURLpp.hpp>
+#include <iomanip>
 #include <iostream>
 #include <sstream>
 
@@ -25,15 +26,24 @@ int main() {
   osBuffer << curlpp::options::Url(urlStrig);
 
   const auto information = osBuffer.str();
-  std::cout << information << std::endl;
   const auto stocksData = parsing::parseStockData(information);
   auto formater = DataFormater();
   std::cout << formater.formatData(stocksData, formatingType::table);
   std::time_t result = std::time(nullptr);
 
   auto date1 = std::asctime(std::localtime(&result));
-  auto unixTime2 = dateToUnixTime(date1);
-  auto date2 = std::asctime(std::localtime(&unixTime2));
-  std::cout << date1 << result << std::endl << date2 << unixTime2 << std::endl;
+  // auto date1 = std::string{"Wed Sep 22 10:27:52 2011"};
+  auto unixTime1 = dateToUnixTime(date1);
+  auto date2 = std::asctime(std::localtime(&unixTime1));
+  auto unixTime2 = dateToUnixTime(date2);
+
+  std::tm local = *std::localtime(&unixTime2);
+
+  std::cout << unixTime1 << std::endl
+            << date1 << std::endl
+            << unixTime2 << std::endl
+            << date2 << std::endl;
+
+  std::cout << "local: " << std::put_time(&local, "%c %Z") << '\n';
   return 0;
 }
